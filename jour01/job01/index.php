910c2e588db5ceb2f444794563e8cde42d63085d
ismail-bouasria <ismail.bouasria@laplateforme.io>
2021-11-02 10:42:41 +0100

    job01 finalisé

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -8,13 +8,20 @@ $str2 = "Vive";
 
 $str3 = "!";
 
-echo $str2.$str.$str3;
+echo $str2." ".$str." ".$str3." ";
 
 $val = 6;
 
 echo $val;
 
-
 echo $val+4;
+
+$mybool = true;
+
+echo $mybool;
+
+$mybool = false;
+
+echo $mybool;
 
 ?>
